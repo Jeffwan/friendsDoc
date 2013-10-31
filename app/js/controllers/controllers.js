009ed5d86625880596b838ebca9a121f7e41205f
Jiaxin Shan <seedjeffwan@gmail.com>
2013-10-30 21:51:06 -0400

    Add Carousel in home page and config CarouselDemoCtrl.

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -42,4 +42,28 @@ angular.module('myApp.controllers', ['ui.bootstrap'])
 
         }])
 
+    .controller('CarouselDemoCtrl',['$scope',function($scope){
+        $scope.myInterval = 5000;
+        $scope.slides = [
+            {
+                image: 'http://2.bp.blogspot.com/-vxx3qRD0-3g/Tq81Im7OvqI/AAAAAAAAKPA/CW6_bj4I8GI/s1600/009.JPG',
+                text: 'first photo'
+            },
+            {
+                image: 'http://www.google.com/hostednews/afp/media/ALeqM5gqtw_XHhAKVrqWoxOoQYEfcw1UqA?size=s3',
+                text: 'second photo'
+            },
+            {
+                image: 'http://2.bp.blogspot.com/-vxx3qRD0-3g/Tq81Im7OvqI/AAAAAAAAKPA/CW6_bj4I8GI/s1600/009.JPG',
+                text: 'third photo'
+            },
+            {
+                image: 'http://www.google.com/hostednews/afp/media/ALeqM5gqtw_XHhAKVrqWoxOoQYEfcw1UqA?size=s3',
+                text: 'fourth photo'
+            }
+        ];
+
+    }])
+
+
 
